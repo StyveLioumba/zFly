8d3d24594fe0255198e1b0f0dfb6fef0ccf56f81
Styve <styvelioumba98@gmail.com>
2020-11-02 21:53:39 +0100

    modification de la recuperation a l'accueil fini

--- app/src/main/java/cg/essengogroup/zfly/view/fragments/AccueilFragment.java
+++ app/src/main/java/cg/essengogroup/zfly/view/fragments/AccueilFragment.java
@@ -6,24 +6,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -36,6 +37,8 @@ import cg.essengogroup.zfly.R;
 import cg.essengogroup.zfly.controller.adapter.MultiViewTypeAdapter;
 import cg.essengogroup.zfly.controller.adapter.SliderPagerAdapter;
 import cg.essengogroup.zfly.controller.adapter.UserAdapter;
+import cg.essengogroup.zfly.controller.utils.Methodes;
+import cg.essengogroup.zfly.controller.utils.VolleySingleton;
 import cg.essengogroup.zfly.model.Model;
 import cg.essengogroup.zfly.model.Slider;
 import cg.essengogroup.zfly.model.User;
@@ -50,13 +53,12 @@ public class AccueilFragment extends Fragment {
     private Context context;
     private Activity activity;
 
-    private RecyclerView recyclerView,recycleH;
+    private RecyclerView recyclerView, recycleH;
     private ProgressBar progressBar;
     private LinearLayout linearLayout;
     private ViewPager viewPager;
     private NestedScrollView nestedScrollView;
 
-    private FirebaseDatabase database;
     private FirebaseUser mUser;
     private FirebaseAuth mAuth;
     private UserAdapter userAdapter;
@@ -68,7 +70,6 @@ public class AccueilFragment extends Fragment {
     private SliderPagerAdapter sliderPagerAdapter;
     private ArrayList<Slider> sliderArrayList;
 
-    private Query referenceUser,referenceModel,referenceSlider;
     private LinearLayoutManager manager;
 
     public AccueilFragment() {
@@ -79,41 +80,34 @@ public class AccueilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        root=inflater.inflate(R.layout.fragment_accueil, container, false);
-        context=getContext();
-        activity=getActivity();
+        root = inflater.inflate(R.layout.fragment_accueil, container, false);
+        context = getContext();
+        activity = getActivity();
 
-        mAuth=FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
-        database=FirebaseDatabase.getInstance();
-        referenceUser=database.getReference("users").orderByChild("isArtiste").equalTo(true).limitToLast(50);
-        referenceModel=database.getReference("post");
-        referenceSlider=database.getReference("slide");
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
-        referenceUser.keepSynced(true);
-        referenceModel.keepSynced(true);
+        userArrayList = new ArrayList<>();
+        modelArrayList = new ArrayList<>();
+        sliderArrayList = new ArrayList<>();
 
-        userArrayList=new ArrayList<>();
-        modelArrayList=new ArrayList<>();
-        sliderArrayList=new ArrayList<>();
+        recyclerView = root.findViewById(R.id.recycleActualite);
+        recycleH = root.findViewById(R.id.recycleArtiste);
+        progressBar = root.findViewById(R.id.progress);
+        linearLayout = root.findViewById(R.id.lineBottom);
+        viewPager = root.findViewById(R.id.viewpager);
+        nestedScrollView = root.findViewById(R.id.nestedscrol);
 
-        recyclerView=root.findViewById(R.id.recycleActualite);
-        recycleH=root.findViewById(R.id.recycleArtiste);
-        progressBar=root.findViewById(R.id.progress);
-        linearLayout=root.findViewById(R.id.lineBottom);
-        viewPager=root.findViewById(R.id.viewpager);
-        nestedScrollView=root.findViewById(R.id.nestedscrol);
-
-        manager=new LinearLayoutManager(context);
+        manager = new LinearLayoutManager(context);
         manager.setStackFromEnd(true);
         manager.setReverseLayout(true);
 
-        root.findViewById(R.id.fab).setOnClickListener(v->startActivity(new Intent(context, PostActivity.class)));
+        root.findViewById(R.id.fab).setOnClickListener(v -> startActivity(new Intent(context, PostActivity.class)));
 
-        root.findViewById(R.id.seeMore).setOnClickListener(v->startActivity(new Intent(context, AllUserActivity.class)));
+        root.findViewById(R.id.seeMore).setOnClickListener(v -> startActivity(new Intent(context, AllUserActivity.class)));
 
 
-        Timer timer= new Timer();
+        Timer timer = new Timer();
         timer.scheduleAtFixedRate(new SliderTimer(), 4000, 6000);
 
         getDataUser();
@@ -123,56 +117,70 @@ public class AccueilFragment extends Fragment {
         return root;
     }
 
-    private void getDataUser(){
+    private void getDataUser() {
+        StringRequest stringRequest = new StringRequest(Request.Method.DEPRECATED_GET_OR_POST, "le lien ici",
+                response -> {
+                    if (!TextUtils.isEmpty(response)) {
+                        userArrayList.clear();
+                        /***
+                         *  le parsing de data se fera ici
+                         ***/
 
-        referenceUser.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                userArrayList.clear();
-                for (DataSnapshot data : dataSnapshot.getChildren()){
-                    User user=data.getValue(User.class);
-                    if (!user.getUser_id().equalsIgnoreCase(mUser.getUid())){
-                        userArrayList.add(user);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+
+                                JSONObject object = jsonArray.getJSONObject(i);
+
+                                User user = new Gson().fromJson(String.valueOf(object), User.class);
+
+                                if (!user.getUser_id().equalsIgnoreCase(mUser.getUid())) {
+                                    userArrayList.add(user);
+                                }
+                            }
+
+                            userAdapter = new UserAdapter(context, userArrayList);
+                            LinearLayoutManager managerH = new LinearLayoutManager(context, RecyclerView.HORIZONTAL, false);
+                            managerH.setReverseLayout(true);
+                            managerH.setStackFromEnd(true);
+                            recycleH.setLayoutManager(managerH);
+                            recycleH.setAdapter(userAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
+                },
+                error -> {
+                    Methodes.volleyError(context, error);
+                });
 
-                userAdapter=new UserAdapter(context,userArrayList);
-                LinearLayoutManager managerH=new LinearLayoutManager(context,RecyclerView.HORIZONTAL,false);
-                managerH.setReverseLayout(true);
-                managerH.setStackFromEnd(true);
-                recycleH.setLayoutManager(managerH);
-                recycleH.setAdapter(userAdapter);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
     }
 
-    private void getDataActualite(){
+    private void getDataActualite() {
 
-        referenceModel.addValueEventListener(new ValueEventListener() {
+        /*referenceModel.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 modelArrayList.clear();
 
-                for (DataSnapshot data : dataSnapshot.getChildren()){
-                    Model model=new Model() ;
-                    model.setUser_id(""+data.child("user_id").getValue());
-                    model.setImage(""+data.child("imagePost").getValue());
-                    model.setAudio(""+data.child("sonPost").getValue());
-                    model.setDescription(""+data.child("description").getValue());
-                    model.setType(Integer.parseInt(""+data.child("type").getValue()));
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    Model model = new Model();
+                    model.setUser_id("" + data.child("user_id").getValue());
+                    model.setImage("" + data.child("imagePost").getValue());
+                    model.setAudio("" + data.child("sonPost").getValue());
+                    model.setDescription("" + data.child("description").getValue());
+                    model.setType(Integer.parseInt("" + data.child("type").getValue()));
                     model.setCreateAt(String.valueOf(data.child("createAt").getValue()));
                     model.setPost_id(data.getKey());
                     modelArrayList.add(model);
                 }
 
-                adapter=new MultiViewTypeAdapter(modelArrayList,context);
+                adapter = new MultiViewTypeAdapter(modelArrayList, context);
                 recyclerView.setLayoutManager(manager);
                 recyclerView.setNestedScrollingEnabled(false);
                 recyclerView.setHasFixedSize(true);
@@ -184,24 +192,60 @@ public class AccueilFragment extends Fragment {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+        });*/
+
+        StringRequest stringRequest = new StringRequest(Request.Method.DEPRECATED_GET_OR_POST, "le lien ici",
+                response -> {
+                    if (!TextUtils.isEmpty(response)) {
+                        /***
+                         * le parsing se fait ici
+                         **/
+
+                        modelArrayList.clear();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject object = jsonArray.getJSONObject(i);
+                                Model model = new Gson().fromJson(String.valueOf(object), Model.class);
+                                modelArrayList.add(model);
+                            }
+
+                            adapter = new MultiViewTypeAdapter(modelArrayList, context);
+                            recyclerView.setLayoutManager(manager);
+                            recyclerView.setNestedScrollingEnabled(false);
+                            recyclerView.setHasFixedSize(true);
+                            recyclerView.setAdapter(adapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, error -> {
+            Methodes.volleyError(context, error);
         });
+
+        VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
     }
 
-    private void getDataSlider(){
-        referenceSlider.addValueEventListener(new ValueEventListener() {
+    private void getDataSlider() {
+        /*referenceSlider.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 sliderArrayList.clear();
-                for (DataSnapshot data : dataSnapshot.getChildren()){
-                    Slider slider=new Slider() ;
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    Slider slider = new Slider();
 
-                    slider.setImage(""+data.child("imageURL").getValue());
-                    slider.setDesignation(""+data.child("text").getValue());
+                    slider.setImage("" + data.child("imageURL").getValue());
+                    slider.setDesignation("" + data.child("text").getValue());
 
                     sliderArrayList.add(slider);
                 }
 
-                sliderPagerAdapter=new SliderPagerAdapter(context,sliderArrayList);
+                sliderPagerAdapter = new SliderPagerAdapter(context, sliderArrayList);
                 viewPager.setAdapter(sliderPagerAdapter);
 
 
@@ -211,17 +255,47 @@ public class AccueilFragment extends Fragment {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });
+        });*/
+
+        StringRequest stringRequest=new StringRequest(Request.Method.DEPRECATED_GET_OR_POST,"le lien ici",
+                response -> {
+                    if (TextUtils.isEmpty(response)){
+                        /***
+                         * le parsing se fait ici
+                         ***/
+                        sliderArrayList.clear();
+                        try {
+                            JSONObject jsonObject=new JSONObject(response);
+                            JSONArray jsonArray=jsonObject.getJSONArray("data");
+
+                            for (int i=0;i<jsonArray.length();i++){
+                                JSONObject object=jsonArray.getJSONObject(i);
+                                Slider slider=new Gson().fromJson(String.valueOf(object),Slider.class);
+
+                                sliderArrayList.add(slider);
+                            }
+
+                            sliderPagerAdapter = new SliderPagerAdapter(context, sliderArrayList);
+                            viewPager.setAdapter(sliderPagerAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                error -> {
+                    Methodes.volleyError(context,error);
+                });
+
+        VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
     }
 
     class SliderTimer extends TimerTask {
-
         @Override
         public void run() {
             activity.runOnUiThread(() -> {
-                if(viewPager.getCurrentItem()< sliderArrayList.size()-1){
-                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
-                }else {
+                if (viewPager.getCurrentItem() < sliderArrayList.size() - 1) {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                } else {
                     viewPager.setCurrentItem(0);
                 }
             });
